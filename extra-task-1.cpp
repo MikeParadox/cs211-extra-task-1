bb2f184b162fa2c63cdc0eec3e94c0f32374fb62
Mikhail <hudih@mail.ru>
2024-10-06 17:00:36 +0300

    added seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -14,3 +14,14 @@ int get_minutes(int time) { return time % num_sec_in_hour / num_sec_in_minute; }
 // returns time in seconds from given time minus whole minutes
 int get_seconds(int time) { return time % num_sec_in_minute; }
 
+// Returns the number of seconds later that a time in seconds time2 than
+// time1
+double seconds_difference(double time_1, double time_2)
+{
+    /*      >>> seconds_difference(1800.0, 3600.0) 1800.0
+            >>> seconds_difference(3600.0, 1800.0) - 1800.0
+            >>> seconds_difference(1800.0, 2160.0) 360.0
+            >>> seconds_difference(1800.0, 1800.0) 0.0*/
+    return time_2 - time_1;
+}
+
