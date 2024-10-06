5251ba6dc092451feb897062a66c3c38d806fd6a
Mikhail <hudih@mail.ru>
2024-10-06 17:17:21 +0300

    added time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -48,3 +48,41 @@ double to_24_hour_clock(double hours)
 
     return std::fmod(hours, num_hours_in_day);
 }
+
+// Returns time at UTC+0, where utc_offset is the number of hours away from UTC
+// +0
+double time_to_utc(int utc_offset, double time)
+{
+    return std::fmod((time - utc_offset), num_hours_in_day);
+}
+
+// double time_from_utc(int utc_offset, double time)
+// {
+//     /*
+//         Return UTC time in time zone utc_offset.
+
+//         >>> time_from_utc(+0, 12.0)
+//         12.0
+
+//         >>> time_from_utc(+1, 12.0)
+//         13.0
+
+//         >>> time_from_utc(-1, 12.0)
+//         11.0
+
+//         >>> time_from_utc(+6, 6.0)
+//         12.0
+
+//         >>> time_from_utc(-7, 6.0)
+//         23.0
+
+//         >>> time_from_utc(-1, 0.0)
+//         23.0
+
+//         >>> time_from_utc(-1, 23.0)
+//         22.0
+
+//         >>> time_from_utc(+1, 23.0)
+//         0.0
+//     */
+// }
