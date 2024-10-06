dabb7b027e5e7af42e14d40c8369120ad39585c5
Mikhail <hudih@mail.ru>
2024-10-06 16:59:19 +0300

    added get_minutes

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -8,3 +8,6 @@ constexpr int num_sec_in_minute{60};
 // returns time in hours from time given in minutes
 int get_hours(int time) { return time / num_sec_in_hour; }
 
+// returns time in minutes from given time minus whole hours
+int get_minutes(int time) { return time % num_sec_in_hour / num_sec_in_minute; }
+
